b14aff70d6c1d280632a0b0191c6a1dc908de8c9
Cardoso <gacds.0308@gmail.com>
2025-06-18 18:18:57 -0300

    Sidebar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import App from './App';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Artist from './pages/Artist';
+import Album from './pages/Album';
+import Playlists from './pages/Playslist';
+import PlaylistView from './pages/PlaylistsView';
+import History from './pages/History';
+import Login from './pages/Login';
+import Sidebar from './components/Sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Sidebar/>}/>
+        <Route path="/search" element={<Search />} />
+        <Route path="/artist/:id" element={<Artist />} />
+        <Route path="/album/:id" element={<Album />} />
+        <Route path="/playlists" element={<Playlists />} />
+        <Route path="/playlist/:id" element={<PlaylistView />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/login" element={<Login />} />
+        
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
