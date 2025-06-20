8a13a340b12c3ea7196be08fe3cc1b7d5ba976dc
Cardoso <gacds.0308@gmail.com>
2025-06-20 15:52:00 -0300

    Botões next e previus do carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,7 +4,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
-import { FreeMode, Pagination } from 'swiper/modules';
+import 'swiper/css/navigation';
+import { FreeMode, Pagination , Navigation } from 'swiper/modules';
 import '../styles/carousel.css';
 
 function Carousel( {resultados = [] , tocarPreview } ){
@@ -12,29 +13,30 @@ function Carousel( {resultados = [] , tocarPreview } ){
     return (
         <div className="carousel-container">
             <Swiper
-                modules={[FreeMode, Pagination]}
+                modules={[FreeMode, Pagination, Navigation]}
                 spaceBetween={16}
                 slidesPerView={'auto'}
                 freeMode={true}
                 grabCursor={true}
                 pagination={false}
+                navigation={true}
             >
             {resultados.map((track, i) => (
-            <SwiperSlide key={i} style={{ width: '200px' }}>
-                <div className="carousel-item">
-                    <div className="card-img">
-                        <img src={track.album.cover_medium} alt={track.title} style={{ width: '100%', borderRadius: '8px' }} />
-                        <div className="play">
-                            <button onClick={() => tocarPreview(track.preview)}>
-                                <span className="fas fa-play"></span>
-                            </button>
+                <SwiperSlide key={i} style={{ width: '200px' }}>
+                    <div className="carousel-item">
+                        <div className="card-img">
+                            <img src={track.album.cover_medium} alt={track.title} style={{ width: '100%', borderRadius: '8px' }} />
+                            <div className="play">
+                                <button onClick={() => tocarPreview(track.preview)}>
+                                    <span className="fas fa-play"></span>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="card-text">
+                            <p className="music-name">{track.title}</p>
                         </div>
                     </div>
-                    <div className="card-text">
-                        <p className="music-name">{track.title}</p>
-                    </div>
-                </div>
-            </SwiperSlide>
+                </SwiperSlide>
             ))}
             </Swiper>
         </div>
